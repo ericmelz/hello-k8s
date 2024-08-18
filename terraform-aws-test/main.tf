2e9c2de6736df35895862e71652350ddbdcd64bc
ericmelz <eric@emelz.com>
2024-08-18 13:38:40 -0700

    terraform

--- terraform-aws-test/main.tf
+++ terraform-aws-test/main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+variable "bucket_name_suffix" {
+  description = "Suffix to append to the bucket name"
+  type        = string
+}
+
 resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-terraform-test-bucket-123456"
+  bucket = "my-terraform-test-bucket-${var.bucket_name_suffix}"
 
   tags = {
     Name        = "MyTestBucket"

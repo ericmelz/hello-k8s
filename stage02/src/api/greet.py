d2d6970eab8c0240ff84f3f01d09fbcc59e6b23e
ericmelz <eric@emelz.com>
2024-08-24 15:53:21 -0700

    work on stage02

---
+++ stage02/src/api/greet.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    greeting: str = "Hello, K8s, from local!"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+app = FastAPI()
+
+@app.get("/greet")
+def greet():
+    return {"message": settings.greeting}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)

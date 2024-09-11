75850c618ae82b405038236dbfaebd3a12df7019
ericmelz <eric@emelz.com>
2024-09-11 10:21:44 -0700

    work

--- stage05/terraform/stage05-rds/main.tf
+++ stage05/terraform/stage05-rds/main.tf
@@ -84,7 +84,7 @@ resource "aws_db_instance" "mysql" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  name                 = var.db_name
+  db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
